5a1c67d6cc1f9a9cad8fd2b69d4064640e143083
Pedko777 <yuriipedko@gmail.com>
2020-06-16 17:13:01 +0300

    Updates

---
+++ precache-manifest.89e9a895f425c4f95eae842f0b885bf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dfbb1b87d17b2afec19e2f83d6fd72e",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "c167cb59fd873473bef4",
+    "url": "/goit-react-hw-01-components/static/css/main.a95f9957.chunk.css"
+  },
+  {
+    "revision": "b75201cc70fe9357a5bf",
+    "url": "/goit-react-hw-01-components/static/js/2.737c316c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.737c316c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c167cb59fd873473bef4",
+    "url": "/goit-react-hw-01-components/static/js/main.795b82fe.chunk.js"
+  },
+  {
+    "revision": "4bc1f32317223384cc74",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.df6ef7f8.js"
+  }
+]);
